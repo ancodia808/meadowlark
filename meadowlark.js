97530147473b6ed62c99a778c5d5cefa86836f3f
Eric Hunter <ancodia@ancodia.net>
2016-09-01 15:11:50 -0500

    Moved fortune functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,21 +1,10 @@
-// Another 'nother 'nother comment...
-// Another 'nother comment...
-// Another comment...
-// This is for the 'issue53' branch...
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple."
-];
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -26,31 +15,17 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  // Output plain text response...
-  //res.type('text/plain');
-  //res.send('Meadowlark Travel');
-
   // Output handlebars response...
   res.render('home');
 })
 
 app.get('/about', function(req, res){
-  // Output plain text response...
-  //res.type('text/plain');
-  //res.send('About Meadowlark Travel');
-
     // Output handlebars response...
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 })
 
 // custom 404 page
 app.use(function(req, res, next){
-  // Output plain text response...
-  //res.type('text/plain');
-  //res.status(404);
-  //res.send('404 - Not Found');
-
   // Output handlebars response...
   res.status(404);
   res.render('404');
@@ -59,11 +34,6 @@ app.use(function(req, res, next){
 // custom 500 page
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-
-  // Output plain text response...
-  //res.type('text/plain');
-  //res.status(500);
-  //res.send('500 - Server Error');
 
   // Output handlebars response...
   res.status(500);
