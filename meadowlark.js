cc32a087800fb3b3105fffd127ecabcc0ebcfaa4
Eric Hunter <ancodia@ancodia.net>
2016-09-01 18:06:03 -0500

    Changes from working on Chapter 5

--- meadowlark.js
+++ meadowlark.js
@@ -4,7 +4,16 @@ var fortune = require('./lib/fortune.js');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+  helpers: {
+      section: function(name, options){
+          if(!this._sections) this._sections = {};
+          this._sections[name] = options.fn(this);
+          return null;
+      }
+  }
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -14,19 +23,38 @@ app.set('port', process.env.PORT || 3000);
 // Set static middleware...
 app.use(express.static(__dirname + '/public'));
 
+// Set middleware for running page tests...
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
+
+
+/// Routes
 app.get('/', function(req, res){
-  // Output handlebars response...
   res.render('home');
-})
+});
 
 app.get('/about', function(req, res){
-    // Output handlebars response...
-    res.render('about', { fortune: fortune.getFortune() });
-})
+    res.render('about',
+               {
+                 fortune: fortune.getFortune(),
+                 pageTestScript: '/qa/tests-about.js'
+               });
+});
+
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+  res.render('tours/request-group-rate');
+});
+
 
 // custom 404 page
 app.use(function(req, res, next){
-  // Output handlebars response...
   res.status(404);
   res.render('404');
 });
@@ -35,7 +63,6 @@ app.use(function(req, res, next){
 app.use(function(err, req, res, next) {
   console.error(err.stack);
 
-  // Output handlebars response...
   res.status(500);
   res.render('500');
 });
