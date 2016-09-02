c05f348224e358d5d3034250fecbd51ae4e22545
Eric Hunter <ancodia@ancodia.net>
2016-09-02 17:07:11 -0500

    Added more complex handler for '/api/tours'

--- meadowlark.js
+++ meadowlark.js
@@ -54,7 +54,36 @@ app.get('/about', function(req, res){
 });
 
 app.get('/api/tours', function(req, res){
-  res.json(tours);
+  var toursXml = '<?xml version="1.0"?><tours>' +
+                 tours.map(function(p){
+                   return '<tour price="' + p.price +
+                          '" id="' + p.id + '">' + p.name + '</tour>'
+                 }).join('') +
+                 '</tours>';
+
+  //console.log('toursXml: ' + toursXml);
+
+  var toursText = tours.map(function(p){
+                    return p.id + ': ' + p.name + ' (' + p.price + ')'
+                  }).join('\n');
+
+  res.format({
+    'application/json': function(){
+      res.json(tours);
+    },
+    'application/xml': function(){
+      res.type('applicaiton/xml');
+      res.send(toursXml);
+    },
+    'text/xml': function(){
+      res.type('text/xml');
+      res.send(toursXml);
+    },
+    'text/plain': function(){
+      res.type('text/plain');
+      res.send(toursText);
+    }
+  });
 });
 
 app.get('/headers', function(req, res){
